75bf18ac22719e0c052ac386804a98171969f52d
biancapower <bianca@biancapower.com>
2022-10-03 14:49:08 +1100

    Refactor plugin to use hook - ensures only runs once, removing need for render_counts hack - runs before pages are generated, so that data is available in time

--- _plugins/jekyll-medusa/jekyll-medusa/get-medusa-data.rb
+++ _plugins/jekyll-medusa/jekyll-medusa/get-medusa-data.rb
@@ -1,29 +1,27 @@
 require 'httparty'
-# FIXME: products page only accesses the generated data files on re-render
-module JekyllMedusa
-  class GetMedusaData < Jekyll::Generator
-    def generate(site)
-      # only run on build (not re-render in local development)
-      @render_count = 1 if (!defined?@render_count)
-      @render_count > 1 ? Jekyll.logger.info('already fetched data') && return : @render_count += 1
 
-      config = site.config['jekyll-medusa'] || {}
+Jekyll::Hooks.register :site, :after_init do |site, payload|
+  def get_data(site)
+    config = site.config['jekyll-medusa'] || {}
 
-      for endpoint in config['medusaEndpoints']['store']['get']
-        url = config['medusaApiUrl'] + "/store/" + endpoint
+    # loop through endpoints and get data
+    for endpoint in config['medusaEndpoints']['store']['get']
+      url = config['medusaApiUrl'] + "/store/" + endpoint
 
-        response = HTTParty.get(url)
+      response = HTTParty.get(url)
 
-        if response.code == 200
-          File.open("_data/generated/#{endpoint}.json", 'w') do |f|
-            f.write(response.body)
-          end
-        else
-          raise "Error retriving #{endpoint}: #{response.code} #{response.message}"
+      if response.code == 200
+        File.open("_data/generated/#{endpoint}.json", 'w') do |f|
+          f.write(response.body)
         end
+      else
+        raise "Error retriving #{endpoint}: #{response.code} #{response.message}"
       end
 
-      Jekyll.logger.info('Data fetched successfully.')
+      Jekyll.logger.info("✅ #{endpoint} data fetched successfully")
     end
+
   end
+
+  get_data(site)
 end
